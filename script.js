17d4e6f8772b113f9df25f6a0320e182dd85a49c
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-07-17 16:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,12 +19,18 @@ class Student extends Person {
 		return this.name + " is studing."
 	}
 }
+var student = new Student("Saksham",15);
+student.study();
 
-class Teacher extends Person {}
-    super(name,age);
+class Teacher extends Person {
+	 super(name,age);
 	teach(){
 		return this.name + " is teaching."
 	}
+}
+   
+var teacher = new Teacher("Manjeet",25);
+teacher.teach();
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
